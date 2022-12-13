5ab1744a3e48a38a78b64985f28bf2c220dad9c9
erickmuriithi <rickymurithii@gmail.com>
2022-12-13 07:16:16 +0300

    add files

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+    validates :title ,presence: true
+    validates :content , length: {minimum: 250}
+    validates :summary , length: {maximum: 250}
+    # validates :category , inclusion: {in: ["Fiction", "Non-Fiction"]}
+    validates :category , inclusion: {in: %w(Fiction Non-Fiction)}
+
+    validate :title_is_clickbait
+
+    def title_is_clickbait
+        unless title == "You Won't Believe These True Facts"
+            errors.add(:title, "title is invalid")
+        end
+    end
 end
